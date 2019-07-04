5bea4c9e38f7a849b97dd40bc9eaecdefb67af2c
Nicholas Stonecipher <nickster258@gmail.com>
2019-07-04 01:13:11 -0400

    Adjusted to double up slab if slab already existed

--- src/main/java/org/stonecipher/slabs/SlabEventHandler.java
+++ src/main/java/org/stonecipher/slabs/SlabEventHandler.java
@@ -1,22 +1,40 @@
 package org.stonecipher.slabs;
 
+import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Slab;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
+import org.bukkit.inventory.meta.ItemMeta;
 import org.stonecipher.Slabs;
 
 public class SlabEventHandler  implements Listener {
 
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event){
-		if(Slabs.isSlab(event.getBlockPlaced().getBlockData().getMaterial())) {
+		Block toPlace = event.getBlockPlaced();
+		BlockData data = toPlace.getBlockData();
+		if(Slabs.isSlab(data.getMaterial())) {
 			if(Slabs.hasSlabLore(event.getItemInHand().getItemMeta())){
-				SetInverted(event.getBlockPlaced());
+				if(!slabExists(event)) {
+					SetInverted(event.getBlockPlaced());
+				} else {
+					Slab slab = (Slab) toPlace.getBlockData();
+					slab.setType(Slab.Type.DOUBLE);
+					toPlace.setBlockData(slab);
+				}
 			}
 		}
+	}
+
+	public boolean slabExists(BlockPlaceEvent event) {
+		if (Slabs.isSlab(event.getBlockReplacedState().getBlockData().getMaterial())) {
+			return true;
+		}
+		return false;
 	}
 	
 	public void SetInverted(Block b){
